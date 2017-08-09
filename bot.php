d3c908e5848d1ee2e79e4c35abffbbc8d47946c6
ChayanonBa <ba.chayanon_st@tni.ac.th>
2017-08-09 03:57:17 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,7 +6,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-
+echo $events;
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
@@ -45,5 +45,5 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK";
-echo $events;
+echo "OK1";
+
