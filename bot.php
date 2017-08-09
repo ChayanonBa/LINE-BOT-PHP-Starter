ca6ee727864923a3ffd8e58c01ab34b66c441c9a
ChayanonBa <ba.chayanon_st@tni.ac.th>
2017-08-09 04:02:14 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,8 +6,9 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-echo $events['events'];
+
 if (!is_null($events['events'])) {
+	echo $events;
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
