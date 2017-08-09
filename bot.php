f5a835d38f72a5515fd8bcb89f9c78800f7251ca
ChayanonBa <ba.chayanon_st@tni.ac.th>
2017-08-09 04:11:25 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,7 +8,7 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 
 if (!is_null($events['events'])) {
-	$x = $events;
+	echo "whatttttttttttttttt";
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
